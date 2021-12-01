0a162c3e2857f41da1a57639997c7a95211dfa62
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2021-12-01 19:46:39 +0300

    add: proc server

---
+++ greet_server/server.go
@@ -0,0 +1,36 @@
+package greetserver
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/keremdokumaci/go-grpc/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.Greeting.GetFirstName()
+	message := "Hello  " + firstName
+	response := &greetpb.GreetResponse{
+		Result: message,
+	}
+
+	return response, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("Failed to serve %v", err)
+	}
+}
